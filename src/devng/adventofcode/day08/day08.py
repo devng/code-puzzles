daa0c86e0e66871904094866d5aa62b751fed836
Nikolay Georgiev <n.g.georgiev@gmail.com>
2015-12-18 20:16:09 +0200

    Provide Python Solution for Advent of Code Day 8

---
+++ src/devng/adventofcode/day08/day08.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# Make the script compatible with python 3.x
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+import re
+
+
+def process_file(filename):
+    result = 0
+    result2 = 0
+    with open(filename, "r") as fin:
+        for line in fin:
+            s = line.strip()
+            r = eval(line) # evaluated, needed for part 1
+            e = re.escape(s) # encoded, needed for part 2
+            # print(type(r), r, len(r), type(e), e, len(e)) # DEBUG
+            result += len(s) - len(r)
+            result2 += len(e) - len(s) + 2 # add
+    return result, result2
+
+
+def main():
+    r = process_file("input.txt")
+    print("Answer part one: %d.\nAnswer part two: %d" % r)
+
+
+if __name__ == "__main__":
+    main()
