6052e5ec2b54a97a6a519ad08e11562be642b278
Nikolay Georgiev <nikolay.georgiev@egym.de>
2020-04-20 21:04:41 +0200

    Simplify codejam 2020 expogo

--- src/devng/codejam2020/expogo.py
+++ src/devng/codejam2020/expogo.py
@@ -1,69 +1,32 @@
 #!/usr/bin/env python3
 
 from collections import deque
+from collections import namedtuple
 
-
-class Node:
-    def __init__(self, x, y, path):
-        self.x = x
-        self.y = y
-        self.path = path
-
-
-    def asTuple(self):
-        return self.x, self.y
-
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-
-    def __repr__(self):
-        return "<Node x={} y={} path={}>".format(self.x, self.y, self.path)
-
+Node = namedtuple("Node", ["x", "y", "path"])
 
 
 def bfs(goal_x, goal_y):
     # we will search for the solution backwards starting from the goal
-    goal = Node(goal_x, goal_y, "")
     Q = deque()
-    Q.append(goal)
+    Q.append(Node(goal_x, goal_y, ""))
     while Q:
-        #print(Q)
-        v = Q.popleft()
-        x, y = v.asTuple()
+        # print(Q)
+        x, y, path = Q.popleft()
         if x == 0 and y == 0:
-            return v
-
+            return path
         # We came from North
         if x % 2 == 0 and (y - 1) % 2 == 0:
-            n = Node(x // 2, (y - 1) // 2, v.path + "N")
-            Q.append(n)
+            Q.append(Node(x // 2, (y - 1) // 2, path + "N"))
         # We came from South
         if x % 2 == 0 and (y + 1) % 2 == 0:
-            n = Node(x // 2, (y + 1) // 2, v.path + "S")
-            Q.append(n)
-         # We came from East
+            Q.append(Node(x // 2, (y + 1) // 2, path + "S"))
+        # We came from East
         if (x - 1) % 2 == 0 and y % 2 == 0:
-            n = Node((x - 1) // 2, y // 2, v.path + "E")
-            Q.append(n)
-         # We came from West
+            Q.append(Node((x - 1) // 2, y // 2, path + "E"))
+        # We came from West
         if (x + 1) % 2 == 0 and y % 2 == 0:
-            n = Node((x + 1) // 2, y // 2, v.path + "W")
-            Q.append(n)
-
-    return None
-
-
-def solve(X, Y):
-    node = bfs(X, Y)
-    if node:
-        return node.path
-
+            Q.append(Node((x + 1) // 2, y // 2, path + "W"))
     return "IMPOSSIBLE"
 
 
@@ -72,5 +35,5 @@ if __name__ == "__main__":
     T = int(input())
     for t in range(1, T + 1):
         X, Y = list(map(int, input().split()))
-        result = solve(X, Y)
+        result = bfs(X, Y)
         print("Case #{}: {}".format(t, result))
